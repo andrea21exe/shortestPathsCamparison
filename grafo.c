fc513ff58ad0199f9ffb5d24dc7f316b00ca7d45
andrea <andreanastasio01@gmail.com>
2024-09-16 00:17:20 +0200

    Fix totale

--- grafo.c
+++ grafo.c
@@ -80,45 +80,6 @@ void stampaGrafo(struct Grafo* grafo) {
     }
 }
 
-int bellmanFord(struct Grafo* grafo, int sorgente){
-
-    //Inizializzazione
-    for(int i = 0; i < grafo->numVertici; i++){
-        grafo->arrayListe[i].distanza = INT_MAX;
-    }
-    grafo->arrayListe[sorgente].distanza = 0;
-    grafo->arrayListe[sorgente].parent = sorgente;
-
-    //Algoritmo
-    for(int j = 0; j < grafo->numVertici; j++){
-        for(int i = 0; i < grafo->numVertici; i++){
-            struct Nodo* arco = grafo->arrayListe[i].testa;
-            while(arco){
-                if(grafo->arrayListe[i].distanza != INT_MAX && grafo->arrayListe[i].distanza + arco->peso < grafo->arrayListe[arco->vertice].distanza){
-                    grafo->arrayListe[arco->vertice].distanza = grafo->arrayListe[i].distanza + arco->peso;
-                    grafo->arrayListe[arco->vertice].parent = i;
-                }
-                arco = arco->prossimo;
-            }
-
-        }
-    }
-
-    //Controllo cicli negativi
-    for(int i = 0; i < grafo->numVertici; i++){
-        struct Nodo* arco = grafo->arrayListe[i].testa;
-        while(arco){
-            if(grafo->arrayListe[i].distanza != INT_MAX && grafo->arrayListe[i].distanza + arco->peso < grafo->arrayListe[arco->vertice].distanza){
-                return -1;
-            }
-            arco = arco->prossimo;
-        }
-
-    }
-
-    return 0;
-}
-
 void exchange(int*v, int *pos, int a, int b){
 
     if (a < 0 || b < 0) {
@@ -182,39 +143,6 @@ int extractMin(struct Grafo* grafo, int* heap, int* pos, int dim) {
 }
 
 
-void dijkstra(struct Grafo* grafo, int sorgente){
-    //Creo il vettore per l'heap +  vettore pos in cui inserisco l'indice in heap di ogni vertice
-    int *heap = (int*)malloc(sizeof(int)*(grafo->numVertici));
-    int *pos = (int*)malloc(sizeof(int)*(grafo->numVertici));
-
-    for(int i = 0; i < grafo->numVertici; i++){
-        heap[i] = i;
-        pos[i] = i;
-        grafo->arrayListe[i].distanza = INT_MAX;
-        grafo->arrayListe[i].parent = -1;
-    }
-
-    grafo->arrayListe[sorgente].distanza = 0;
-    grafo->arrayListe[sorgente].parent = sorgente;   
-
-    makeMinHeap(grafo, heap, pos, grafo->numVertici);
-    int dim = grafo->numVertici - 1;
-    
-
-    while(dim > 0){
-        int vertice = extractMin(grafo, heap, pos, dim--);
-        struct Nodo* arco = grafo->arrayListe[vertice].testa;
-        while(arco){
-            if(grafo->arrayListe[vertice].distanza != INT_MAX && grafo->arrayListe[vertice].distanza + arco->peso < grafo->arrayListe[arco->vertice].distanza){
-                grafo->arrayListe[arco->vertice].parent = vertice;
-                decreaseKey(grafo, heap, pos, arco->vertice, grafo->arrayListe[vertice].distanza + arco->peso);
-            }   
-            arco = arco->prossimo;
-        }
-    }
-
-}
-
 // Funzione per verificare se un arco esiste già
 int esisteArco(struct Grafo* grafo, int sorgente, int destinazione) {
     struct Nodo* arcoCorrente = grafo->arrayListe[sorgente].testa;
@@ -227,10 +155,30 @@ int esisteArco(struct Grafo* grafo, int sorgente, int destinazione) {
     return 0;  // Arco non esistente
 }
 
-// Funzione per aggiungere archi casuali
+// Funzione per creare una componente fortemente connessa di base
+void connettiFortemente(struct Grafo* grafo) {
+    for (int i = 0; i < grafo->numVertici - 1; i++) {
+        int peso = rand() % 100 + 1;  // Pesi tra 1 e 100
+        if (esisteArco(grafo, i, i + 1) == 0) {
+            aggiungiArco(grafo, i, i + 1, peso);
+        }
+    }
+    
+    // Aggiungi un arco dall'ultimo vertice al primo per chiudere il ciclo
+    int peso = rand() % 100 + 1;
+    if (esisteArco(grafo, grafo->numVertici - 1, 0) == 0) {
+        aggiungiArco(grafo, grafo->numVertici - 1, 0, peso);
+    }
+}
+
+// Funzione modificata per aggiungere archi casuali e garantire la forte connessione
 void aggiungiArchi(struct Grafo* grafo, int numArchi) {
     srand(0);  // Inizializza il generatore di numeri casuali
 
+    // Prima creiamo una componente fortemente connessa di base
+    connettiFortemente(grafo);
+
+    // Poi aggiungiamo archi casuali per il resto del grafo
     for (int i = 0; i < numArchi; i++) {
         int sorgente = rand() % grafo->numVertici;
         int destinazione = rand() % grafo->numVertici;
@@ -244,6 +192,7 @@ void aggiungiArchi(struct Grafo* grafo, int numArchi) {
         }
     }
 }
+
 
 struct GrafoMatrice* creaGrafoMatrice(int numVertici){
     struct GrafoMatrice* grafo = (struct GrafoMatrice*)malloc(sizeof(struct GrafoMatrice));
@@ -289,7 +238,7 @@ void stampaGrafoMatrice(struct GrafoMatrice* grafo) {
 }
 
 int* floydWarshall(struct GrafoMatrice *grafo){
-    //Primo indice: 1 o 2, secondo indice: sorgente, terzo indice: colonna
+    // Primo indice: 1 o 2, secondo indice: sorgente, terzo indice: destinazione
     int ***listaMatrici = (int***)malloc(sizeof(int**) * 2);
     for(int i = 0; i < 2; i++){
         listaMatrici[i] = (int**)malloc(sizeof(int*) * grafo->numVertici);
@@ -298,24 +247,19 @@ int* floydWarshall(struct GrafoMatrice *grafo){
         }
     }
 
-    //Copia la matrice di adiacenza nella struttura
+    // Copia la matrice di adiacenza nella struttura
     for(int i = 0; i < grafo->numVertici; i++){
         for(int j = 0; j < grafo->numVertici; j++){
             listaMatrici[0][i][j] = grafo->matrice[i][j];
         }
     }
 
-    //Le matrici vengono riutilizzate
+    // Le matrici vengono riutilizzate
     int index;
     for(int i = 0; i < grafo->numVertici; i++){
         index = (i + 1) % 2;
         for(int j = 0; j < grafo->numVertici; j++){
-            listaMatrici[index][i][j] = listaMatrici[index ^ 1][i][j];
-            listaMatrici[index][j][i] = listaMatrici[index ^ 1][j][i];
-        }
-        for(int j = 0; j < grafo->numVertici; j++){
             for(int k = 0; k < grafo->numVertici; k++){
-                //Sceglie il minore
                 if(listaMatrici[index ^ 1][j][i] != INT_MAX && listaMatrici[index ^ 1][i][k] != INT_MAX && listaMatrici[index ^ 1][j][k] > listaMatrici[index ^ 1][j][i] + listaMatrici[index ^ 1][i][k]){
                     listaMatrici[index][j][k] = listaMatrici[index ^ 1][j][i] + listaMatrici[index ^ 1][i][k];
                 } else {
@@ -342,21 +286,138 @@ int* floydWarshall(struct GrafoMatrice *grafo){
 
     int *v = (int*)malloc(sizeof(int) * 3);
     v[0] = max; v[1] = source; v[2] = destination;
+
     return v;
 }
 
+void dijkstra(struct Grafo* grafo, int sorgente) {
+    int numVertici = grafo->numVertici;
+    
+    // Inizializza le distanze e l'heap
+    int* heap = (int*)malloc(numVertici * sizeof(int));
+    int* pos = (int*)malloc(numVertici * sizeof(int));
+    
+    for (int i = 0; i < numVertici; i++) {
+        grafo->arrayListe[i].distanza = INT_MAX; 
+        grafo->arrayListe[i].parent = -1; 
+        heap[i] = i;
+        pos[i] = i;
+    }
+    
+    // La distanza dalla sorgente a se stessa è 0. E' l'unica = 0. Le altre sono INT_MAX
+    decreaseKey(grafo, heap, pos, sorgente, 0);
+    
+    int dim = numVertici;
+    while (dim > 0) {
+        int u = extractMin(grafo, heap, pos, dim);
+        dim--;
+        
+        struct Nodo* nodo = grafo->arrayListe[u].testa;
+        while (nodo != NULL) {
+            int v = nodo->vertice;
+            int peso = nodo->peso;
+            
+            // Relaxation
+            if (grafo->arrayListe[u].distanza != INT_MAX && grafo->arrayListe[v].distanza > grafo->arrayListe[u].distanza + peso) {
+                decreaseKey(grafo, heap, pos, v, grafo->arrayListe[u].distanza + peso);
+                grafo->arrayListe[v].parent = u;
+            }
+            nodo = nodo->prossimo;
+        }
+    }
+
+    // Stampa dei risultati
+    /*
+    printf("Nodo\tDistanza dalla sorgente%d \tPercorso\n", sorgente);
+    for (int i = 0; i < numVertici; i++) {
+        printf("%d\t%d\t\t\t", i, grafo->arrayListe[i].distanza);
+        int parent = grafo->arrayListe[i].parent;
+        while (parent != -1) {
+            printf("%d <- ", parent);
+            parent = grafo->arrayListe[parent].parent;
+        }
+        printf("N/A\n");
+    }
+    */
+
+    free(heap);
+    free(pos);
+}
+
+int bellmanFord(struct Grafo* grafo, int sorgente) {
+    int numVertici = grafo->numVertici;
+
+    // Inizializza le distanze e i parent all'interno delle strutture del grafo
+    for (int i = 0; i < numVertici; i++) {
+        grafo->arrayListe[i].distanza = INT_MAX;  
+        grafo->arrayListe[i].parent = -1;         
+    }
+    grafo->arrayListe[sorgente].distanza = 0;     
+
+    for (int i = 0; i < numVertici - 1; i++) {
+        for (int u = 0; u < numVertici; u++) {
+            struct Nodo* nodo = grafo->arrayListe[u].testa;
+            while (nodo != NULL) {
+                int v = nodo->vertice;
+                int peso = nodo->peso;
+
+                // Relax
+                if (grafo->arrayListe[u].distanza != INT_MAX && grafo->arrayListe[v].distanza > grafo->arrayListe[u].distanza + peso) {
+                    grafo->arrayListe[v].distanza = grafo->arrayListe[u].distanza + peso;
+                    grafo->arrayListe[v].parent = u;
+                }
+                nodo = nodo->prossimo;
+            }
+        }
+    }
+
+    // Cicli Negativi?
+    for (int u = 0; u < numVertici; u++) {
+        struct Nodo* nodo = grafo->arrayListe[u].testa;
+        while (nodo != NULL) {
+            int v = nodo->vertice;
+            int peso = nodo->peso;
+            if (grafo->arrayListe[u].distanza != INT_MAX && grafo->arrayListe[v].distanza > grafo->arrayListe[u].distanza + peso) {
+                return -1;
+            }
+            nodo = nodo->prossimo;
+        }
+    }
+
+    // Stampa dei risultati
+    /*
+    printf("Nodo\tDistanza dalla sorgente%d \tPercorso\n", sorgente);
+    for (int i = 0; i < numVertici; i++) {
+        printf("%d\t%d\t\t\t", i, grafo->arrayListe[i].distanza);
+        int parent = grafo->arrayListe[i].parent;
+        while (parent != -1) {
+            printf("%d <- ", parent);
+            parent = grafo->arrayListe[parent].parent;
+        }
+        printf("N/A\n");
+    }
+    */
+
+   return 0;
+}
+
+
 int main() {
     
-    //Creazione grafo ----
-    int numVertici = 1000;  
-    int numArchi = 4000;    
+    // Creazione grafo ----
+    int numVertici = 100;  
+    int numArchi = 400;    
 
     struct Grafo* grafo = creaGrafo(numVertici);
     aggiungiArchi(grafo, numArchi);
 
-    //Per debug, voglio vedere se ottengo gli stessi risultati    
-    int* bfVector = (int*)malloc(sizeof(int)*grafo->numVertici);
-    int* dijkstraVector = (int*)malloc(sizeof(int)*grafo->numVertici);
+    // Per debug, voglio vedere se ottengo gli stessi risultati    
+    int** bfVector = (int**)malloc(sizeof(int*)*grafo->numVertici);
+    int** dijkstraVector = (int**)malloc(sizeof(int*)*grafo->numVertici);
+    for(int i = 0; i < grafo->numVertici; i++){
+        bfVector[i] = (int*)malloc(sizeof(int)*grafo->numVertici);
+        dijkstraVector[i] = (int*)malloc(sizeof(int)*grafo->numVertici);
+    }
 
     struct GrafoMatrice* grafoMatrice = creaGrafoMatrice(numVertici);;
     copiaGrafo(grafoMatrice, grafo);
@@ -365,6 +426,7 @@ int main() {
     int size2 = sizeof(struct GrafoMatrice) + sizeof(int*) * grafoMatrice->numVertici + sizeof(int) * grafoMatrice->numVertici * grafoMatrice->numVertici;
     printf("Dimensione liste di adiacenza: %d B --- Dimensione matrice: %d B\n", size1, size2);
 
+    // Floyd Warshall
     printf("Floyd Warshall...\n");
     double fwTime;
     time_t inizio3 = clock();
@@ -375,44 +437,48 @@ int main() {
     //stampaGrafo(grafo);
     //stampaGrafoMatrice(grafoMatrice);
 
-    //Dijkstra
+    // Dijkstra
     printf("Dijkstra...\n");
     double dTime;
     time_t inizio2 = clock();
     int source;
     for(source = 0; source < grafo->numVertici; source++){
         dijkstra(grafo, source);
+        for(int i = 0; i < grafo->numVertici; i++){
+            dijkstraVector[source][i] = grafo->arrayListe[i].distanza;
+        }
     }
     time_t fine2 = clock();
     dTime =  (double)(fine2 - inizio2)/CLOCKS_PER_SEC;
-
-    for(int i = 0; i < grafo->numVertici; i++){
-        int distanza = grafo->arrayListe[i].distanza;
-        int parent = grafo->arrayListe[i].parent;
-        dijkstraVector[i] = grafo->arrayListe[i].distanza;
-        //printf("Distanza da %d a %d: %d, Parent: %d\n", source, i, distanza, parent);
-    }
     
-    //Bellman-Ford
+    // Bellman-Ford
     double bfTime;
     printf("Bellman Ford...\n");
     time_t inizio = clock();
     for(source = 0; source < grafo->numVertici; source++){
-        int bf = bellmanFord(grafo, source);
+        bellmanFord(grafo, source);
+        for(int i = 0; i < grafo->numVertici; i++){
+            bfVector[source][i] = grafo->arrayListe[i].distanza;
+        }
     }
     time_t fine = clock();
-    bfTime =  (double)(fine - inizio)/CLOCKS_PER_SEC;
+    bfTime = (double)(fine - inizio)/CLOCKS_PER_SEC;
 
-    
+    int max = INT_MIN;
     for(int i = 0; i < grafo->numVertici; i++){
-        int distanza = grafo->arrayListe[i].distanza;
-        int parent = grafo->arrayListe[i].parent;
-        bfVector[i] = grafo->arrayListe[i].distanza;
-        if(bfVector[i] != dijkstraVector[i]){
-            return -2;
+        for(int j = 0; j < grafo->numVertici; j++){
+            if(dijkstraVector[i][j] != bfVector[i][j]){
+                printf("%d %d\n", i,j);
+            }
+            if(max < dijkstraVector[i][j]){
+                max = dijkstraVector[i][j];
+            }
         }
-        //printf("Distanza da %d a %d: %d, Parent: %d\n", source, i, distanza, parent);
     }
+    if(max != v[0]){
+        printf("\nNOOOOO");
+        return -1;
+    } 
 
     printf("Max Path: Source: %d, Destination: %d, Max Length: %d", v[1], v[2], v[0]);
     printf("\nFloyd Warshall: %fs --- Bellman Ford: %fs --- Dijkstra: %fs\n", fwTime, bfTime, dTime);
